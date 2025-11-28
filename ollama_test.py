e5fd585e8bb606425addb0d82dcf26f3bf842c1d
Hirotaka532 <codingsparring514@gmail.com>
2025-11-28 19:36:47 -0400

    Delete ollama_test.py

--- ollama_test.py
+++
@@ -1,7 +0,0 @@
-from langchain_ollama import OllamaLLM # pip install langchain langchain-ollama ollama
-
-model = OllamaLLM(model="llama3.2") # Descargar Ollama (https://ollama.com/) y descargar Llama3.2 con el comando: ollama pull llama3.2 
-
-result = model.invoke(input="waddup")
-
-print(result)
