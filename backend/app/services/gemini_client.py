677b8cc5191ad7e36e233c2fb9652002c916342a
pulidoarroyo <pulidoarroyo24@gmail.com>
2025-11-29 18:48:18 -0400

    fix: gemini model

--- backend/app/services/gemini_client.py
+++ backend/app/services/gemini_client.py
@@ -6,7 +6,7 @@ from app.config import settings
 
 genai.configure(api_key=settings.GEMINI_API_KEY)
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 def ask_gemini(prompt: str):
     # Personalización del chatbot
