60033d34861e2aa4c8af77e0383baf2ad2f21d9c
AnahDev <anadevtools@gmail.com>
2025-12-01 13:43:01 -0400

    Agregando primeros ep de usuario

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,8 @@
 
 from fastapi import FastAPI
 from app.routers.chatbot import router as chatbot_router
+from app.routers.user import router as user_router
+
 
 app = FastAPI(
     title="Academic Chatbot API",
@@ -10,6 +12,8 @@ app = FastAPI(
 )
 
 app.include_router(chatbot_router)
+
+app.include_router(user_router, prefix="/users", tags=["Users"])
 
 @app.get("/")
 def root():
